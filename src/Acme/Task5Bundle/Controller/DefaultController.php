9e172a866ab879fe4c68174f4479df2665029670
karmeljuk <karmeljuk@gmail.com>
2013-11-14 00:03:24 +0200

    Firts pages templates

--- src/Acme/Task5Bundle/Controller/DefaultController.php
+++ src/Acme/Task5Bundle/Controller/DefaultController.php
@@ -6,8 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('AcmeTask5Bundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('AcmeTask5Bundle:Default:index.html.twig',
+            array(
+                'name'=>'Krom',
+                'dog_breed'=>'Caucasian Shepherd Dog',
+                'dog_age'=>'1 year',
+            )
+        );
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('AcmeTask5Bundle:Default:about.html.twig',
+            array(
+                'name'=>'Krom',
+                'dog_breed'=>'Caucasian Shepherd Dog',
+                'dog_age'=>'1 year',
+            )
+        );
+    }
+
+    public function contactAction()
+    {
+        return $this->render('AcmeTask5Bundle:Default:contact.html.twig',
+            array(
+                'name'=>'Krom',
+                'dog_breed'=>'Caucasian Shepherd Dog',
+                'dog_age'=>'1 year',
+            )
+        );
     }
 }
