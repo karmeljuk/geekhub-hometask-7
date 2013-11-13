06e88bd452549bcb8ae7f21410a62a498dc1972c
karmeljuk <karmeljuk@gmail.com>
2013-11-13 22:44:33 +0200

    Create template layout.html.twig

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Acme\Task5Bundle\AcmeTask5Bundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
