606e4cda19e7c9c80cfe9e5b3e014117c6ee1562
karmeljuk <karmeljuk@gmail.com>
2013-11-14 20:45:04 +0200

    Creates simple pages

--- src/Acme/Task5Bundle/Controller/DefaultController.php
+++ src/Acme/Task5Bundle/Controller/DefaultController.php
@@ -3,6 +3,12 @@
 namespace Acme\Task5Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Acme\DemoBundle\Form\ContactType;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
@@ -10,9 +16,12 @@ class DefaultController extends Controller
     {
         return $this->render('AcmeTask5Bundle:Default:index.html.twig',
             array(
-                'name'=>'Krom',
-                'dog_breed'=>'Caucasian Shepherd Dog',
-                'dog_age'=>'1 year',
+                'page_name'=>'index',
+                'tent_name'=>'Imogene UL2 Tent',
+                'price'=>'$249.99',
+                'description'=>'Our lightest weight free-standing tent ever. Roomy enough for two, light enough for one.
+                Setting the standard for ultra-lightweight livable space. 2013 Backpacker Magazine Gear Guide Pick for
+                 Best Freestanding Ultralight Tent.',
             )
         );
     }
@@ -21,9 +30,10 @@ class DefaultController extends Controller
     {
         return $this->render('AcmeTask5Bundle:Default:about.html.twig',
             array(
-                'name'=>'Krom',
-                'dog_breed'=>'Caucasian Shepherd Dog',
-                'dog_age'=>'1 year',
+                'page_name'=>'about',
+                'description'=>'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+                commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
+                malesuada magna mollis euismod. Donec sed odio dui.',
             )
         );
     }
@@ -32,9 +42,11 @@ class DefaultController extends Controller
     {
         return $this->render('AcmeTask5Bundle:Default:contact.html.twig',
             array(
-                'name'=>'Krom',
-                'dog_breed'=>'Caucasian Shepherd Dog',
-                'dog_age'=>'1 year',
+                'page_name'=>'contact',
+                'description'=>'Cras vulputate in felis at egestas. Mauris ut diam risus. Sed in dapibus sapien.
+                Vestibulum viverra ut ligula in mattis. Proin in lacus pellentesque, tincidunt sem id, gravida urna.
+                Donec lacus risus, imperdiet a purus non, tristique aliquam leo. Nam nec tincidunt sem, eu pulvinar
+                erat. Nulla sed risus eu est blandit iaculis eget vel eros.'
             )
         );
     }
